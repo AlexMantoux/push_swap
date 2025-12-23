4d4501a0b7d73c55903655ed17245a64312203a2
Alex Mantoux <alexmantoux@mac.home>
2025-12-23 12:17:10 +0100

    Fix push and linked_list func

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amantoux <amantoux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 09:05:44 by alexmantoux       #+#    #+#             */
-/*   Updated: 2025/12/23 11:50:32 by amantoux         ###   ########.fr       */
+/*   Updated: 2025/12/23 12:11:28 by amantoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,14 @@
 int main (int argc, char **argv)
 {
     t_list  *lst_a;
+    t_list  *lst_b;
     int i;
 
     i = 1;
     if (argc > 1)
     {
         lst_a = ft_lstnew(ft_atoi(argv[i]));
+        lst_b = NULL;
         i++;
         while (argv[i])
         {
@@ -28,9 +30,17 @@ int main (int argc, char **argv)
             i++;
         }
         ft_lstdisplay(&lst_a, 'a');
+        ft_lstdisplay(&lst_b, 'b');
+        printf("______________________________\n");
         swap_a(&lst_a);
         ft_lstdisplay(&lst_a, 'a');
-
+        ft_lstdisplay(&lst_b, 'b');
+        printf("______________________________\n");
+        push_b(&lst_a, &lst_b);
+        push_b(&lst_a, &lst_b);
+        push_b(&lst_a, &lst_b);
+        ft_lstdisplay(&lst_a, 'a');
+        ft_lstdisplay(&lst_b, 'b');
     }
     return (0);
 }
