f25b3eef6dc50bbf3d04c2b4f69f881499f49ab9
Alex Mantoux <alexmantoux@Mac.lan>
2026-01-23 18:52:04 +0100

    Update

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: amantoux <amantoux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 11:16:16 by rtrutall          #+#    #+#             */
-/*   Updated: 2026/01/22 15:25:09 by amantoux         ###   ########.fr       */
+/*   Updated: 2026/01/22 15:25:36 by amantoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int	main(int argc, char **argv)
 	normalize_indices(lst_a);
 	flag_execution(flags, &lst_a, &lst_b, &bench);
 	// ft_lstdisplay(&lst_a, 'a');
-	// ft_lstdisplay(&lst_b, 'b');
+	// ft_lstdisplay(&lst_b, 'b');c
 	if (flags.bench)
 		display_bench(&bench, disorder);
 }
